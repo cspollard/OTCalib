cc1027cb841e38603929d2563badf7352b6409e7
Chris Pollard <cpollard@naf-atlas-gpu01.desy.de>
2020-10-16 16:23:21 +0200

    fix two bugs

--- square.py
+++ square.py
@@ -2,38 +2,28 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
 import numpy as np
+from time import time
 
 print("torch version:", torch.__version__)
 
 device="cuda"
 outprefix="square/"
 
-ndata = 100000
+ndata = 1000
 nmc = 20*ndata
-epochsize = 100000 # 2**12
-nthetas = 1
-nepochs = 2**18
+epochsize = nmc
+nthetas = 0
+nepochs = 2**16
 
 ncritic = 10
-lr = 5e-4
+lr = 5e-5
 lam = 0
 wgan = True
 rmsprop = True
 cycle = False # True
 lrdecay = 0 # 1e-5
-testthetas = torch.randn((10, 1, nthetas), device=device).repeat((1, nmc, 1))
+testthetas = torch.randn((32, 1, nthetas), device=device).repeat((1, nmc, 1))
 
-
-alldata = torch.rand((ndata,), device=device)
-alldata *= alldata
-alldatacpu = alldata.detach().cpu().squeeze()
-
-allmc = torch.sort(torch.rand((nmc,), device=device))[0]
-allmccpu = allmc.detach().cpu().squeeze()
-truecpu = allmccpu*allmccpu
-
-alldata = alldata.view((ndata,1))
-allmc = allmc.view((nmc,1))
 
 def histcurve(bins, fills, default):
   xs = [x for x in bins for _ in (1, 2)]
@@ -44,336 +34,363 @@ def histcurve(bins, fills, default):
 def tonp(xs):
   return xs.cpu().detach().numpy()
 
+
 def combine(seq):
   return torch.cat((torch.mean(seq, 0), torch.std(seq, 0)), axis=0)
 
 
-name = \
-  "deeperstiller_nongauss_batched_%d_datasamps_%d_mcsamps_%d_thetas_%d_ncritic_%.2e_lr_%0.2e_lambda" \
-    % (ndata, nmc, nthetas, ncritic, lr, lam)
-
-if cycle:
-  name = "onecycle_" + name
-elif lrdecay:
-  name = name + "%0.2e_lrdecay" % lrdecay
-
-if rmsprop:
-  name = "rmsprop_" + name
-
-if wgan:
-  name = "wgan_" + name
-
-name += "_%d_epochs" % nepochs
-
-name += "_%d_epochsize" % epochsize
-
-writer = SummaryWriter(outprefix + name)
-
-transport = \
-  torch.nn.Sequential(
-    torch.nn.Linear(1+nthetas, 32)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(32, 32)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(32, 32)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(32, 32)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(32, 32)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(32, 1)
-  )
-
-
 def trans(nn, xs):
-  # original = xs[:,0:1]
-  # thetas = xs[:,1:]
+  original = xs[:,0:1]
+  thetas = xs[:,1:]
 
-  # tmp = nn(original)
+  tmp = nn(original)
 
-  # cv = tmp[:,0:1] # central value
-  # var = tmp[:,1:] # eigen variations
-  # coeffs = var - cv
+  cv = tmp[:,0:1] # central value
+  var = tmp[:,1:] # eigen variations
+  coeffs = var - cv
 
-  # corr = torch.bmm(thetas.unsqueeze(1), coeffs.unsqueeze(2))
+  corr = torch.bmm(thetas.unsqueeze(1), coeffs.unsqueeze(2))
 
-  # return cv + corr.squeeze(2)
+  return cv + corr.squeeze(2)
 
-  return nn(xs)
-
-
-critic = \
-  torch.nn.Sequential(
-    torch.nn.Linear(1, 32)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(32, 32)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(32, 32)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(32, 32)
-  )
+  # return nn(xs)
 
 
-phi = \
-  torch.nn.Sequential(
-    torch.nn.Linear(64, 64)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(64, 64)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(64, 64)
-  , torch.nn.LeakyReLU(inplace=True)
-  , torch.nn.Linear(64, 1)
-  )
-
-transport.to(device)
-critic.to(device)
-phi.to(device)
-
-
-if rmsprop:
-  toptim = torch.optim.RMSprop(transport.parameters(), lr=lr)
-  aoptim = torch.optim.RMSprop(list(critic.parameters()) + list(phi.parameters()), lr=lr)
-else:
-  toptim = torch.optim.SGD(transport.parameters(), lr=lr)
-  aoptim = torch.optim.SGD(list(critic.parameters()) + list(phi.parameters()), lr=lr)
-
-if cycle:
-  tsched = torch.optim.lr_scheduler.OneCycleLR(
-      toptim
-    , lr
-    , total_steps=nepochs
-    , cycle_momentum=False
-    , pct_start=0.1
+for lab in [str(x) for x in range(10)]:
+  transport = \
+    torch.nn.Sequential(
+      torch.nn.Linear(1, 64)
+    , torch.nn.LeakyReLU(inplace=True)
+    , torch.nn.Linear(64, 64)
+    , torch.nn.LeakyReLU(inplace=True)
+    , torch.nn.Linear(64, 64)
+    , torch.nn.LeakyReLU(inplace=True)
+    , torch.nn.Linear(64, 1+nthetas)
     )
 
-  asched = torch.optim.lr_scheduler.OneCycleLR(
-      aoptim
-    , lr
-    , total_steps=nepochs
-    , cycle_momentum=False
-    , pct_start=0.1
+
+  # start from ~the identity
+  transport[-1].weight.data *= 0.01
+  transport[-1].bias.data *= 0.01
+
+
+  critic = \
+    torch.nn.Sequential(
+      torch.nn.Linear(1, 64)
+    , torch.nn.LeakyReLU(inplace=True)
+    , torch.nn.Linear(64, 64)
+    , torch.nn.LeakyReLU(inplace=True)
+    , torch.nn.Linear(64, 64)
+    , torch.nn.LeakyReLU(inplace=True)
+    , torch.nn.Linear(64, 1)
     )
 
-elif lrdecay:
-  tsched = torch.optim.lr_scheduler.ExponentialLR(toptim, 1-lrdecay)
-  asched = torch.optim.lr_scheduler.ExponentialLR(aoptim, 1-lrdecay)
 
-for epoch in range(nepochs):
+  phi = \
+    torch.nn.Sequential(
+      torch.nn.Linear(2, 64)
+    , torch.nn.LeakyReLU(inplace=True)
+    , torch.nn.Linear(64, 64)
+    , torch.nn.LeakyReLU(inplace=True)
+    , torch.nn.Linear(64, 1)
+    )
+
+  transport.to(device)
+  critic.to(device)
+  phi.to(device)
+
+
+  if rmsprop:
+    toptim = torch.optim.RMSprop(transport.parameters(), lr=lr)
+    aoptim = torch.optim.RMSprop(list(critic.parameters()) + list(phi.parameters()), lr=lr)
+  else:
+    toptim = torch.optim.SGD(transport.parameters(), lr=lr)
+    aoptim = torch.optim.SGD(list(critic.parameters()) + list(phi.parameters()), lr=lr)
+
   if cycle:
-    lr = asched.get_last_lr()[0]
+    tsched = torch.optim.lr_scheduler.OneCycleLR(
+        toptim
+      , lr
+      , total_steps=nepochs
+      , cycle_momentum=False
+      , pct_start=0.1
+      )
+
+    asched = torch.optim.lr_scheduler.OneCycleLR(
+        aoptim
+      , lr
+      , total_steps=nepochs
+      , cycle_momentum=False
+      , pct_start=0.1
+      )
+
   elif lrdecay:
-    lr *= (1-lrdecay)
+    tsched = torch.optim.lr_scheduler.ExponentialLR(toptim, 1-lrdecay)
+    asched = torch.optim.lr_scheduler.ExponentialLR(aoptim, 1-lrdecay)
 
-  if epoch > 0 and epoch % 10 == 0:
-    print("epoch", epoch)
-
-    fig = plt.figure(figsize=(6, 6))
-
-    thetas = torch.zeros((nmc, nthetas), device=device)
-    mc1 = torch.cat((allmc, thetas), axis=1)
-    nomcpu = (allmc + trans(transport, mc1)).detach().cpu().squeeze()
+  starttime = time()
+  plottime = 0
 
 
-    variations = []
-    for i in range(testthetas.size()[0]):
-      mc1 = torch.cat((allmc, testthetas[i]), axis=1)
-      transported = (trans(transport, mc1) + allmc).detach().cpu().squeeze()
-      variations.append(transported)
+  alldata = torch.rand((ndata,), device=device)
+  alldata *= alldata
+  alldatacpu = alldata.detach().cpu().squeeze()
+
+  allmc = torch.sort(torch.rand((nmc,), device=device))[0]
+  allmccpu = allmc.detach().cpu().squeeze()
+  truecpu = allmccpu*allmccpu
+
+  alldata = alldata.view((ndata,1))
+  allmc = allmc.view((nmc,1))
 
 
-    bins = [-0.05] + [x*0.05 for x in range(21)]
-    hs, bins, _ = \
-      plt.hist(
-        [ allmccpu.clamp(-0.01, 1.01)
-        , alldatacpu.clamp(-0.01, 1.01)
-        , nomcpu.clamp(-0.01, 1.01)
-        , truecpu.clamp(-0.01, 1.01)
-        ]
-      , bins=bins
-      , density=True
-      )
+  name = \
+    "repeatthetas_gauss_batched_deeper_wider_onecritic_%d_datasamps_%d_mcsamps_%d_thetas_%d_ncritic_%.2e_lr_%0.2e_lambda" \
+      % (ndata, nmc, nthetas, ncritic, lr, lam)
 
-    hvars, _, _ = \
-      plt.hist(
-        list(map(lambda h: h.clamp(-0.01, 1.01).numpy(), variations))
-      , bins=bins
-      , density=True
-      )
+  if cycle:
+    name = "onecycle_" + name
+  elif lrdecay:
+    name = name + "%0.2e_lrdecay" % lrdecay
 
-    fig.clear()
+  if rmsprop:
+    name = "rmsprop_" + name
 
-    varcurves = []
-    for h in hvars:
-      varcurves.append(histcurve(bins, h, 0)[1])
+  if wgan:
+    name = "wgan_" + name
 
-    (xs, hmc) = histcurve(bins, hs[0], 0)
-    (xs, hdata) = histcurve(bins, hs[1], 0)
-    (xs, htrans) = histcurve(bins, hs[2], 0)
-    (xs, htrue) = histcurve(bins, hs[3], 0)
+  name += "_%d_epochs" % nepochs
 
-    plt.scatter(
-        (bins[:-1] + bins[1:]) / 2.0
-      , hs[1]
-      , label="observed target"
-      , color='black'
-      , linewidth=0
-      , marker='o'
-      , zorder=5
-      )
+  name += "_%d_epochsize" % epochsize
 
-    plt.plot(
-        xs
-      , hmc
-      , linewidth=2
-      , color="red"
-      , linestyle="dashed"
-      , label="original prediction"
-      , zorder=3
-      )
+  name += "_" + lab
+
+  writer = SummaryWriter(outprefix + name)
+
+  for epoch in range(nepochs):
+    if cycle:
+      lr = asched.get_last_lr()[0]
+    elif lrdecay:
+      lr *= (1-lrdecay)
+
+    if epoch > 0 and epoch % 1000 == 0:
+      startplot = time()
+      print("epoch", epoch)
+
+      fig = plt.figure(figsize=(6, 6))
+
+      thetas = torch.zeros((nmc, nthetas), device=device)
+      mc1 = torch.cat((allmc, thetas), axis=1)
+      nomcpu = (allmc + trans(transport, mc1)).detach().cpu().squeeze()
 
 
-    plt.plot(
-        xs
-      , htrans
-      , linewidth=2
-      , color="black"
-      , label="transported prediction"
-      , zorder=2
-      )
+      variations = []
+      for i in range(testthetas.size()[0]):
+        mc1 = torch.cat((allmc, testthetas[i]), axis=1)
+        transported = (trans(transport, mc1) + allmc).detach().cpu().squeeze()
+        variations.append(transported)
 
-    plt.plot(
-        xs
-      , htrue
-      , linewidth=2
-      , linestyle="dotted"
-      , color="blue"
-      , label="true target"
-      , zorder=4
-      )
 
-    for curve in varcurves:
-      plt.plot(xs, curve, color="gray", alpha=0.25, zorder=1)
+      bins = [-0.05] + [x*0.05 for x in range(21)]
+      hs, bins, _ = \
+        plt.hist(
+          [ allmccpu.clamp(-0.01, 1.01)
+          , alldatacpu.clamp(-0.01, 1.01)
+          , nomcpu.clamp(-0.01, 1.01)
+          , truecpu.clamp(-0.01, 1.01)
+          ]
+        , bins=bins
+        , density=True
+        )
 
-    fig.legend()
+      hvars, _, _ = \
+        plt.hist(
+          list(map(lambda h: h.clamp(-0.01, 1.01).numpy(), variations))
+        , bins=bins
+        , density=True
+        )
 
-    plt.xlim(-0.1, 1.1)
-    plt.ylim(0, 6)
+      fig.clear()
 
-    writer.add_figure("hist", fig, global_step=epoch)
+      varcurves = []
+      for h in hvars:
+        varcurves.append(histcurve(bins, h, 0)[1])
 
-    fig.clear()
+      (xs, hmc) = histcurve(bins, hs[0], 0)
+      (xs, hdata) = histcurve(bins, hs[1], 0)
+      (xs, htrans) = histcurve(bins, hs[2], 0)
+      (xs, htrue) = histcurve(bins, hs[3], 0)
 
-    idxs = torch.sort(torch.randint(nmc, (1024,)))[0]
+      plt.scatter(
+          (bins[:-1] + bins[1:]) / 2.0
+        , hs[1]
+        , label="observed target"
+        , color='black'
+        , linewidth=0
+        , marker='o'
+        , zorder=5
+        )
 
-    plt.plot(
-        allmccpu[idxs]
-      , truecpu[idxs]
-      , color="red"
-      , label="true transport vector"
-      , alpha=0.75
-      , fillstyle=None
-      , linewidth=2
-      , zorder=3
-    )
+      plt.plot(
+          xs
+        , hmc
+        , linewidth=2
+        , color="black"
+        , linestyle="dotted"
+        , label="original prediction"
+        , zorder=3
+        )
 
-    plt.plot(
-        allmccpu[idxs]
-      , nomcpu[idxs]
-      , color="blue"
-      , label="nominal transport vector"
-      , alpha=0.75
-      , linewidth=2
-      , fillstyle=None
-      , zorder=2
-    )
 
-    res = 0
-    for variation in variations:
+      plt.plot(
+          xs
+        , htrans
+        , linewidth=2
+        , color="blue"
+        , label="transported prediction"
+        , zorder=2
+        )
+
+      plt.plot(
+          xs
+        , htrue
+        , linewidth=2
+        , linestyle="dashed"
+        , color="red"
+        , label="true target"
+        , zorder=4
+        )
+
+      for curve in varcurves:
+        plt.plot(xs, curve, color="blue", alpha=0.1, zorder=1)
+
+      fig.legend()
+
+      plt.xlim(-0.1, 1.1)
+      plt.ylim(0, 6)
+
+      writer.add_figure("hist", fig, global_step=epoch)
+
+      fig.clear()
+
+      idxs = torch.sort(torch.randint(nmc, (1024,)))[0]
+
       plt.plot(
           allmccpu[idxs]
-        , variation[idxs]
-        , color="gray"
-        , alpha=0.25
+        , truecpu[idxs]
+        , color="red"
+        , label="true transport vector"
+        , alpha=0.75
         , fillstyle=None
-        , zorder=1
+        , linewidth=2
+        , zorder=3
       )
 
-      res += torch.mean((variation - truecpu)**2).item()
+      plt.plot(
+          allmccpu[idxs]
+        , nomcpu[idxs]
+        , color="blue"
+        , label="transport vector"
+        , alpha=0.75
+        , linewidth=2
+        , fillstyle=None
+        , zorder=2
+      )
 
-    plt.xlim(-0.1, 1.1)
-    plt.ylim(-0.1, 1.1)
-    plt.legend()
+      res = 0
+      for variation in variations:
+        plt.plot(
+            allmccpu[idxs]
+          , variation[idxs]
+          , color="blue"
+          , alpha=0.1
+          , fillstyle=None
+          , zorder=1
+        )
 
-    writer.add_figure("trans", fig, global_step=epoch)
+        res += torch.mean((variation - truecpu)**2).item()
 
-    writer.add_scalar("transport_residual", np.sqrt(res) / len(variations), global_step=epoch)
+      plt.xlim(-0.1, 1.1)
+      plt.ylim(-0.1, 1.1)
+      plt.legend()
 
-    plt.close()
+      writer.add_figure("trans", fig, global_step=epoch)
 
-  data = alldata[torch.randint(ndata, (epochsize,), device=device)]
-  # mc = allmc[torch.randint(nmc, (20*epochsize,), device=device)]
-  # thetas = torch.randn((20*epochsize, nthetas), device=device)
-  mc = allmc
-  thetas = torch.randn((nmc, nthetas), device=device)
+      writer.add_scalar("transport_residual", np.sqrt(res) / len(variations), global_step=epoch)
 
-  real = phi(combine(critic(data)))
+      plt.close()
+      plottime += time() - startplot
+      totaltime = time() - starttime
 
-  mc1 = torch.cat((mc, thetas), axis=1)
-  transmc = mc + trans(transport, mc1)
-  fake = phi(combine(critic(transmc)))
+      print(\
+          "fraction of time plotting: %.2e" \
+            % (plottime / (time() - starttime))
+        )
 
-  if wgan:
-    advloss = fake - real
-  else:
-    advloss = \
-      torch.binary_cross_entropy_with_logits(fake, torch.zeros_like(fake)) \
-      + torch.binary_cross_entropy_with_logits(real, torch.ones_like(real))
+    data = alldata[torch.randint(ndata, (epochsize,), device=device)]
+    # mc = allmc[torch.randint(nmc, (20*epochsize,), device=device)]
+    # thetas = torch.randn((20*epochsize, nthetas), device=device)
+    mc = allmc
 
-  advloss.backward()
-  aoptim.step()
+    real = phi(combine(critic(data)))
 
-  writer.add_scalar('advloss', advloss.item(), epoch)
-  # writer.add_scalar('meancriticdata', combdata[0].item(), epoch)
-  # writer.add_scalar('stdcriticdata', combdata[1].item(), epoch)
-  # writer.add_scalar('meancriticmc', combmc[0].item(), epoch)
-  # writer.add_scalar('stdcriticmc', combmc[1].item(), epoch)
-  writer.add_scalar('phidata', real.item(), epoch)
-  writer.add_scalar('phimc', fake.item(), epoch)
-  writer.add_scalar('phidiff', real.item() - fake.item(), epoch)
+    thetas = torch.randn((1, nthetas), device=device).repeat((nmc, 1))
+    mc1 = torch.cat((mc, thetas), axis=1)
+    transmc = mc + trans(transport, mc1)
+    fake = phi(combine(critic(transmc)))
 
-  toptim.zero_grad()
-  aoptim.zero_grad()
+    if wgan:
+      advloss = fake - real
+    else:
+      advloss = \
+        torch.binary_cross_entropy_with_logits(fake, torch.zeros_like(fake)) \
+        + torch.binary_cross_entropy_with_logits(real, torch.ones_like(real))
 
-  for p in list(critic.parameters()) + list(phi.parameters()):
-    p.data.clamp_(-0.1, 0.1)
+    advloss.backward()
+    aoptim.step()
 
-  writer.add_scalar('learningrate', lr, epoch)
+    writer.add_scalar('advloss', advloss.item(), epoch)
+    # writer.add_scalar('meancriticdata', combdata[0].item(), epoch)
+    # writer.add_scalar('stdcriticdata', combdata[1].item(), epoch)
+    # writer.add_scalar('meancriticmc', combmc[0].item(), epoch)
+    # writer.add_scalar('stdcriticmc', combmc[1].item(), epoch)
+    writer.add_scalar('phidata', real.item(), epoch)
+    writer.add_scalar('phimc', fake.item(), epoch)
+    writer.add_scalar('phidiff', real.item() - fake.item(), epoch)
 
-  if cycle or lrdecay:
-    asched.step()
-    tsched.step()
+    toptim.zero_grad()
+    aoptim.zero_grad()
 
-  if epoch % ncritic != 0:
-    continue
+    for p in list(critic.parameters()) + list(phi.parameters()):
+      p.data.clamp_(-0.1, 0.1)
 
-  delta = trans(transport, mc1)
-  distloss = torch.mean(torch.abs(delta))
+    writer.add_scalar('learningrate', lr, epoch)
 
-  fake = phi(combine(critic(mc + delta)))
-  if wgan:
-    fakeloss = -fake
-  else:
-    fakeloss = torch.binary_cross_entropy_with_logits(fake, torch.ones_like(fake))
+    if cycle or lrdecay:
+      asched.step()
+      tsched.step()
 
-  transloss = fakeloss + lam * distloss
+    if epoch % ncritic != 0:
+      continue
 
-  transloss.backward()
-  toptim.step()
+    delta = trans(transport, mc1)
+    distloss = torch.mean(torch.abs(delta))
 
-  writer.add_scalar('distloss', distloss.item(), epoch)
-  writer.add_scalar('fakeloss', fakeloss.item(), epoch)
-  writer.add_scalar('transloss', transloss.item(), epoch)
+    fake = phi(combine(critic(mc + delta)))
+    if wgan:
+      fakeloss = -fake
+    else:
+      fakeloss = torch.binary_cross_entropy_with_logits(fake, torch.ones_like(fake))
+
+    transloss = fakeloss + lam * distloss
+
+    transloss.backward()
+    toptim.step()
+
+    writer.add_scalar('distloss', distloss.item(), epoch)
+    writer.add_scalar('fakeloss', fakeloss.item(), epoch)
+    writer.add_scalar('transloss', transloss.item(), epoch)
 
 
-  toptim.zero_grad()
-  aoptim.zero_grad()
+    toptim.zero_grad()
+    aoptim.zero_grad()
