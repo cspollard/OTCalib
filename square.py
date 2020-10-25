e2bc1655a6a35d5660a5839a3b59b7c7b74f2d8f
Chris Pollard <cpollard@cern.ch>
2020-10-25 18:56:03 +0100

    this is finaly looking good...

--- square.py
+++ square.py
@@ -3,26 +3,36 @@ from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
 import numpy as np
 from time import time
+from sys import argv
 
 print("torch version:", torch.__version__)
 
 device="cuda"
 outprefix="square/"
 
-ndata = 256
-nmc = ndata*64
-epochsize = ndata
-nthetas = 4
-nepochs = 2**18
+
+nthetas = int(argv[2])
+ndata = int(argv[1])
+label = argv[3]
+nmc = 2**17
+batchsize = 1024
+epochsize = 2**11
+nepochs = 256
 
 ncritic = 10
-lr = 3e-5
+lr = 5e-5
 lam = 0
-wgan = False
-rmsprop = True
+wgan = True
+optim = "adam"
 cycle = False # True
-lrdecay = 0 # 1e-5
-testthetas = torch.randn((32, 1, nthetas), device=device).repeat((1, nmc, 1))
+lrdecay = 0 # 1e-3
+
+testthetas = torch.zeros((2*nthetas, 1, nthetas), device=device)
+for i in range(nthetas):
+  testthetas[i,:,i] = 1
+  testthetas[nthetas+i,:,i] = -1
+
+testthetas = testthetas.repeat((1, nmc, 1))
 
 
 def histcurve(bins, fills, default):
@@ -42,55 +52,62 @@ def combine(seq):
 
 
 def trans(nn, xs):
+  # return torch.zeros((xs.size()[0], 1), device=device)
+  # return nn(xs)
+
   original = xs[:,0:1]
   thetas = xs[:,1:]
+  tmp = nn(original)
 
-  # SQUARE
-  # tmp = nn(original)
-
-  # cv = tmp[:,0:1] # central value
-  # var = tmp[:,1:] # eigen variations
-  # ENDSQUARE
-  coeffs = nn(original)
+  centralvalue = tmp[:,0:1]
+  variations = tmp[:,1:]
+  coeffs = variations - centralvalue.repeat((1, nthetas)) # eigen variations
 
   corr = torch.bmm(thetas.unsqueeze(1), coeffs.unsqueeze(2))
 
-  return corr.squeeze(2)
-
-  # return nn(xs)
+  return centralvalue + corr.squeeze(2)
 
 
-for lab in [str(x) for x in range(1)]:
+alldata = torch.randn((ndata,), device=device) / 4 + 0.5
+# alldata.clamp_(0, 1 - 1e-8)
+# SQUARE
+# alldata *= alldata
+alldatacpu = alldata.detach().cpu().squeeze()
+
+allmc = torch.sort(torch.randn((nmc,), device=device))[0] / 4 + 0.5
+# allmc.clamp_(0, 1 - 1e-8)
+allmccpu = allmc.detach().cpu().squeeze()
+# SQUARE
+# truecpu = allmccpu*allmccpu
+truecpu = allmccpu
+
+alldata = alldata.view((ndata,1))
+allmc = allmc.view((nmc,1))
+
+
+
+for lab in [str(x) for x in range(0, 5)]:
   transport = \
     torch.nn.Sequential(
-      torch.nn.Linear(1, 64)
+      torch.nn.Linear(1, 256)
     , torch.nn.LeakyReLU(inplace=True)
-    , torch.nn.Linear(64, 64)
-    , torch.nn.LeakyReLU(inplace=True)
-    , torch.nn.Linear(64, 64)
-    , torch.nn.LeakyReLU(inplace=True)
-    , torch.nn.Linear(64, 64)
-    , torch.nn.LeakyReLU(inplace=True)
-    , torch.nn.Linear(64, nthetas)
+    , torch.nn.Linear(256, 1+nthetas)
     )
 
-
+  # transport = torch.nn.Identity()
+  
   # start from ~the identity
-  transport[-1].weight.data *= 0.01
-  transport[-1].bias.data *= 0.01
+  # transport[-1].weight.data *= 0.01
+  # transport[-1].bias.data *= 0.01
 
 
   critic = \
     torch.nn.Sequential(
-      torch.nn.Linear(1, 64)
+      torch.nn.Linear(1, 256)
     , torch.nn.LeakyReLU(inplace=True)
-    , torch.nn.Linear(64, 64)
+    , torch.nn.Linear(256, 256)
     , torch.nn.LeakyReLU(inplace=True)
-    , torch.nn.Linear(64, 64)
-    , torch.nn.LeakyReLU(inplace=True)
-    , torch.nn.Linear(64, 64)
-    , torch.nn.LeakyReLU(inplace=True)
-    , torch.nn.Linear(64, 1)
+    , torch.nn.Linear(256, 1)
     )
 
 
@@ -113,12 +130,18 @@ for lab in [str(x) for x in range(1)]:
   phi.to(device)
 
 
-  if rmsprop:
+  if optim == "rmsprop":
     toptim = torch.optim.RMSprop(transport.parameters(), lr=lr)
     aoptim = torch.optim.RMSprop(list(critic.parameters()) + list(phi.parameters()), lr=lr)
-  else:
+  elif optim == "adam":
+    toptim = torch.optim.Adam(transport.parameters(), lr=lr)
+    aoptim = torch.optim.Adam(list(critic.parameters()) + list(phi.parameters()), lr=lr)
+  elif optim == "sgd":
     toptim = torch.optim.SGD(transport.parameters(), lr=lr)
     aoptim = torch.optim.SGD(list(critic.parameters()) + list(phi.parameters()), lr=lr)
+  else:
+    print("Error: unrecognized optim")
+    exit(-1)
 
   if cycle:
     tsched = torch.optim.lr_scheduler.OneCycleLR(
@@ -145,23 +168,9 @@ for lab in [str(x) for x in range(1)]:
   plottime = 0
 
 
-  alldata = torch.randn((epochsize,), device=device) / 4 + 0.5
-  # SQUARE
-  # alldata *= alldata
-  alldatacpu = alldata.detach().cpu().squeeze()
-
-  allmc = torch.sort(torch.randn((nmc,), device=device))[0] / 4 + 0.5
-  allmccpu = allmc.detach().cpu().squeeze()
-  # SQUARE
-  # truecpu = allmccpu*allmccpu
-  truecpu = allmccpu
-
-  alldata = alldata.view((ndata,1))
-  allmc = allmc.view((nmc,1))
-
 
   name = \
-    "identity_repeatthetas_gauss_deeeeeep_onlycritic_%d_datasamps_%d_mcsamps_%d_thetas_%d_ncritic_%.2e_lr_%0.2e_lambda" \
+    "identity_gaussuncert_3lay256_critic_2lay256_transport_%d_datasamps_%d_mcsamps_%d_thetas_%d_ncritic_%.2e_lr_%0.2e_lambda" \
       % (ndata, nmc, nthetas, ncritic, lr, lam)
 
   if cycle:
@@ -169,8 +178,7 @@ for lab in [str(x) for x in range(1)]:
   elif lrdecay:
     name = name + "%0.2e_lrdecay" % lrdecay
 
-  if rmsprop:
-    name = "rmsprop_" + name
+  name = optim + "_" + name
 
   if wgan:
     name = "wgan_" + name
@@ -179,7 +187,11 @@ for lab in [str(x) for x in range(1)]:
 
   name += "_%d_epochsize" % epochsize
 
+  name += "_%d_batchsize" % batchsize
+
   name += "_" + lab
+
+  name += "_" + label
 
   writer = SummaryWriter(outprefix + name)
 
@@ -189,7 +201,7 @@ for lab in [str(x) for x in range(1)]:
     elif lrdecay:
       lr *= (1-lrdecay)
 
-    if epoch > 0 and epoch % 1000 == 0:
+    if epoch > 0 and epoch % 1 == 0:
       startplot = time()
       print("epoch", epoch)
 
@@ -199,16 +211,27 @@ for lab in [str(x) for x in range(1)]:
       mc1 = torch.cat((allmc, thetas), axis=1)
       nom = allmc + trans(transport, mc1)
       nomcpu = nom.detach().squeeze().cpu()
+      del mc1, thetas
 
       variations = []
-      for i in range(testthetas.size()[0]):
+      for i in range(nthetas):
         mc1 = torch.cat((allmc, testthetas[i]), axis=1)
-        variations.append(trans(transport, mc1) + allmc)
+        variations.append(
+            (trans(transport, mc1) + allmc).detach().squeeze().cpu()
+          )
+
+        mc1 = torch.cat((allmc, testthetas[nthetas+i]), axis=1)
+        variations.append(
+            (trans(transport, mc1) + allmc).detach().squeeze().cpu()
+          )
+        del mc1
+
+      thetas = torch.randn((nmc, nthetas), device=device)
+      mc1 = torch.cat((allmc, thetas), axis=1)
+      critmc = critic(trans(transport, mc1) + allmc).detach().squeeze().cpu()
+      critdata = critic(alldata).detach().squeeze().cpu()
       del mc1
 
-      mcinputs = torch.cat([nom] + variations, axis=0)
-      critmc = critic(mcinputs).detach().squeeze().cpu()
-      critdata = critic(data).detach().squeeze().cpu()
 
       _, _, _ = \
         plt.hist(
@@ -217,22 +240,12 @@ for lab in [str(x) for x in range(1)]:
         , density=True
         )
 
+      del critmc, critdata
+
       fig.legend()
       writer.add_figure("critichist", fig, global_step=epoch)
 
       fig.clear()
-
-      _, _, _ = \
-        plt.hist(
-          [torch.exp(critmc), torch.exp(critdata)]
-        , label=["exp(critic output) MC", "exp(critic output) data"]
-        , density=True
-        )
-
-      fig.legend()
-      writer.add_figure("expcritichist", fig, global_step=epoch)
-
-      variations = [v.detach().squeeze().cpu() for v in variations]
 
       bins = [-0.05] + [x*0.05 for x in range(22)]
       hs, bins, _ = \
@@ -253,7 +266,11 @@ for lab in [str(x) for x in range(1)]:
         , density=True
         )
 
+
       fig.clear()
+
+      if nthetas == 0:
+        hvars = []
 
       varcurves = []
       for h in hvars:
@@ -305,7 +322,7 @@ for lab in [str(x) for x in range(1)]:
         )
 
       for curve in varcurves:
-        plt.plot(xs, curve, color="blue", alpha=0.1, zorder=1)
+        plt.plot(xs, curve, color="green", alpha=0.5, zorder=1)
 
       fig.legend()
 
@@ -341,17 +358,19 @@ for lab in [str(x) for x in range(1)]:
       )
 
       res = 0
+      uncert = 0
       for variation in variations:
         plt.plot(
             allmccpu[idxs]
           , variation[idxs]
-          , color="blue"
-          , alpha=0.1
+          , color="green"
+          , alpha=0.25
           , fillstyle=None
           , zorder=1
         )
 
-        res += torch.mean((variation - truecpu)**2).item()
+        uncert += ((variation - nomcpu)**2).detach()
+        res += ((variation - truecpu)**2).detach()
 
       plt.xlim(-0.1, 1.1)
       plt.ylim(-0.1, 1.1)
@@ -359,7 +378,18 @@ for lab in [str(x) for x in range(1)]:
 
       writer.add_figure("trans", fig, global_step=epoch)
 
-      writer.add_scalar("transport_residual", np.sqrt(res) / len(variations), global_step=epoch)
+      writer.add_scalar( \
+          "transport_uncertainty"
+        , np.sqrt(torch.mean(uncert).item())
+        , global_step=epoch
+        )
+
+      writer.add_scalar( \
+          "transport_residual"
+        , np.sqrt(torch.mean(res).item())
+        , global_step=epoch
+        )
+
 
       plt.close()
       plottime += time() - startplot
@@ -370,77 +400,86 @@ for lab in [str(x) for x in range(1)]:
             % (plottime / (time() - starttime))
         )
 
-    data = alldata[torch.randint(ndata, (epochsize,), device=device)]
-    # mc = allmc[torch.randint(nmc, (20*epochsize,), device=device)]
-    # thetas = torch.randn((20*epochsize, nthetas), device=device)
-    mc = allmc
+    realmeansum = 0
+    fakemeansum = 0
+    realvarsum = 0
+    fakevarsum = 0
+    advlosssum = 0
 
-    toptim.zero_grad()
-    aoptim.zero_grad()
+    for batch in range(epochsize):
+      data = alldata[torch.randint(ndata, (batchsize,), device=device)]
+      mc = allmc[torch.randint(nmc, (batchsize,), device=device)]
+      # mc = allmc
 
-    real = combine(critic(data))
+      toptim.zero_grad()
+      aoptim.zero_grad()
 
-    thetas = torch.randn((1, nthetas), device=device).repeat((nmc, 1))
-    mc1 = torch.cat((mc, thetas), axis=1)
-    transmc = mc + trans(transport, mc1)
-    fake = combine(critic(transmc))
+      real = critic(data)
+      realmeansum += torch.mean(real).item()
+      realvarsum += torch.var(real).item()
 
-    if wgan:
-      advloss = (real[0] - fake[0])**2 + (real[1] - fake[1])**2
-    else:
-      advloss = \
-        torch.binary_cross_entropy_with_logits(fake, torch.zeros_like(fake)) \
-        + torch.binary_cross_entropy_with_logits(real, torch.ones_like(real))
+      thetas = torch.randn((batchsize, nthetas), device=device)
+      mc1 = torch.cat((mc, thetas), axis=1)
 
-    advloss.backward()
-    aoptim.step()
+      fake = critic(mc + trans(transport, mc1))
+      fakemeansum += torch.mean(fake).item()
+      fakevarsum += torch.var(fake).item()
 
-    writer.add_scalar('advloss', advloss.item(), epoch)
-    # writer.add_scalar('meancriticdata', combdata[0].item(), epoch)
-    # writer.add_scalar('stdcriticdata', combdata[1].item(), epoch)
-    # writer.add_scalar('meancriticmc', combmc[0].item(), epoch)
-    # writer.add_scalar('stdcriticmc', combmc[1].item(), epoch)
-    writer.add_scalar('phidata', torch.mean(real).item(), epoch)
-    writer.add_scalar('phimc', torch.mean(fake).item(), epoch)
-    writer.add_scalar('phidiff', torch.mean(real).item() - torch.mean(fake).item(), epoch)
+      if wgan:
+        w2dist = \
+          (torch.mean(fake) - torch.mean(real))**2 \
+          + (torch.std(fake) - torch.std(real))**2
 
-    toptim.zero_grad()
-    aoptim.zero_grad()
+        advloss = -w2dist
 
-    if wgan:
-      for p in list(critic.parameters()) + list(phi.parameters()):
-        p.data.clamp_(-0.1, 0.1)
+      else:
+        advloss = \
+          torch.binary_cross_entropy_with_logits(fake, torch.zeros_like(fake)) \
+          + torch.binary_cross_entropy_with_logits(real, torch.ones_like(real))
+
+      advloss.backward()
+
+      aoptim.step()
+
+      advlosssum += advloss.item()
+
+      toptim.zero_grad()
+      aoptim.zero_grad()
+
+      if wgan:
+        for p in list(critic.parameters()) + list(phi.parameters()):
+          p.data.clamp_(-0.1, 0.1)
+
+      if batch % ncritic != 0:
+        continue
+
+      real = critic(data)
+
+      fake = critic(mc + trans(transport, mc1))
+
+      if wgan:
+        w2dist = \
+          (torch.mean(fake) - torch.mean(real))**2 \
+          + (torch.std(fake) - torch.std(real))**2
+
+        transloss = w2dist
+
+      else:
+        transloss = torch.binary_cross_entropy_with_logits(fake, torch.ones_like(fake))
+
+      transloss.backward()
+      toptim.step()
+
+      toptim.zero_grad()
+      aoptim.zero_grad()
+
+
+    writer.add_scalar('advloss', advlosssum / epochsize, epoch)
+    writer.add_scalar('meanphidiff', (realmeansum - fakemeansum) / epochsize , epoch)
+    writer.add_scalar('stdphidiff', (np.sqrt(realvarsum) - np.sqrt(fakevarsum)) / epochsize , epoch)
 
     writer.add_scalar('learningrate', lr, epoch)
 
     if cycle or lrdecay:
       asched.step()
       tsched.step()
-
-    if epoch % ncritic != 0:
-      continue
-
-    real = combine(critic(data))
-
-    delta = trans(transport, mc1)
-    distloss = torch.mean(torch.abs(delta))
-
-    fake = combine(critic(mc + delta))
-
-    if wgan:
-      fakeloss = - ((real[0] - fake[0])**2 + (real[1] - fake[1])**2)
-    else:
-      fakeloss = torch.binary_cross_entropy_with_logits(fake, torch.ones_like(fake))
-
-    transloss = fakeloss + lam * distloss
-
-    transloss.backward()
-    toptim.step()
-
-    writer.add_scalar('distloss', distloss.item(), epoch)
-    writer.add_scalar('fakeloss', fakeloss.item(), epoch)
-    writer.add_scalar('transloss', transloss.item(), epoch)
-
-
-    toptim.zero_grad()
-    aoptim.zero_grad()
